e62d08c50c2b91c6b2cb5c70ada851f2d3c823c6
TheMostOriginalNamePossible <44122656+TheMostOriginalNamePossible@users.noreply.github.com>
2024-05-31 20:48:10 -0400

    Session 3 finished

--- combat.py
+++ combat.py
@@ -1,15 +1,20 @@
+"""combat.py
+I just pulled from the assignment. I changed the Monster to Joshua and changed some stats
+"""
+
+
 import tbc
 
 
 def main():
     hero = tbc.Character()
-    hero.name = "Hero"
-    hero.hitPoints = 10
-    hero.hitChance = 50
-    hero.maxDamage = 5
-    hero.armor = 2
+    hero.name = "Sunny"
+    hero.hitPoints = 3000
+    hero.hitChance = 90
+    hero.maxDamage = 600
+    hero.armor = 100
 
-    monster = tbc.Character("Monster", 20, 30, 5, 0)
+    monster = tbc.Character("Joshua", 6000, 85, 300, 200)
 
     hero.printStats()
     monster.printStats()
--- tbc.py
+++ tbc.py
@@ -3,6 +3,7 @@ Turn Based Combat system
 """
 import random
 
+
 class Character(object):
     def __init__(self, name="hero", hp=50, hc=20, md=10, armor=5):
         super().__init__()
@@ -45,7 +46,6 @@ class Character(object):
         else:
             self.__hitChance = 20
 
-
     @property
     def maxDamage(self):
         return self.__maxDamage
@@ -68,7 +68,6 @@ class Character(object):
         else:
             self.__armor = 5
 
-
     def printStats(self):
         print(self.name)
         print("Hit Points: " + str(self.hitPoints))
@@ -76,25 +75,26 @@ class Character(object):
         print("Max Damage: " + str(self.maxDamage))
         print("Armor: " + str(self.armor) + "\n")
 
-
     def hit(self, value):
         if type(value) == Character:
             hitRoll = random.randint(1, 100)
+            print(f"{self.name} attacks...")
             if hitRoll <= self.hitChance:
                 damage = random.randint(1, self.maxDamage)
                 if damage <= value.armor:
                     print(value.name + "'s armor deflected the damage.\n")
                 else:
                     value.hitPoints = value.hitPoints - (damage - value.armor)
-                    print(value.name + " hit for " + str(damage - value.armor) + "\n")
+                    print(self.name + " hit for " + str(damage - value.armor) + "\n")
             else:
                 print(self.name + " missed.\n")
+
 
 def fight(x,y):
     inCombat = True
     while inCombat:
         if x.hitPoints > 0:
-            stopper = input("type anything to continue ")
+            input("type anything to continue ")
             print("")
             x.hit(y)
             print(x.name + ": " + str(x.hitPoints))
@@ -103,7 +103,7 @@ def fight(x,y):
                 print(x.name + " wins")
                 inCombat = False
         if y.hitPoints > 0:
-            stopper = input("type anything to continue ")
+            input("type anything to continue ")
             print("")
             y.hit(x)
             print(x.name + ": " + str(x.hitPoints))
